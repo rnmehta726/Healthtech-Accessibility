2170778653aa8a712168f54d13afd72960607978
Neena Varanasi <68971089+Neena05@users.noreply.github.com>
2024-11-09 16:29:29 -0600

    Delete als-healthtech-accessibility/types directory
    Fix directories

--- als-healthtech-accessibility/types/types.ts
+++
@@ -1,6 +0,0 @@
-// src/types/types.ts
-export type RootStackParamList = {
-  Home: { newPage?: { id: number; title: string; buttons: string[] } };
-  EditPage: undefined;
-  DynamicPage: { pageId: number; title: string; buttons: string[] };
-};
